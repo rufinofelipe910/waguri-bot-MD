fe00fa8b7777cc7d246d346ea19a9d1808d2e949
DuarteXV <danielalejandrobasado@gmail.com>
2026-06-06 09:06:16 -0500

    Actualizar eval.js

--- plugins/owner/eval.js
+++ plugins/owner/eval.js
@@ -1,10 +1,13 @@
 export default {
   name: ["eval", "exec", ">"],
   description: "Evalúa código JavaScript",
+  category: "owner",
   ownerOnly: true,
+
   async run({ reply, text, sock, from, msg }) {
     try {
-      let result = await eval(`(async () => { ${text} })()`);
+      let result = await eval(`(async () => { return ${text} })()`);
+      if (result === undefined) result = "undefined"
       if (typeof result !== "string") result = JSON.stringify(result, null, 2);
       await reply({ text: `✅ *Resultado:*\n\`\`\`${result}\`\`\`` });
     } catch (e) {
