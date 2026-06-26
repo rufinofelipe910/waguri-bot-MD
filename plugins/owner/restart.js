f03e2b01a2a389084c286b4a852ae7b1d467e910
DuarteXV <danielalejandrobasado@gmail.com>
2026-06-25 22:02:23 -0500

    Actualizar restart.js

--- plugins/owner/restart.js
+++ plugins/owner/restart.js
@@ -1,9 +1,9 @@
 export default { 
-  name: "restart", "rest", 
+  name: ["restart", "rest"], 
   description: "Reinicia el bot", 
   ownerOnly: true,
   async run({ conn, m, reply }) {
-    await reply({'📍 *Reiniciando el bot, espere un momento...*'})
+    await reply('📍 *Reiniciando el bot, espere un momento...*')
     setTimeout(() => { process.exit(1) }, 1000)
   }
 }
